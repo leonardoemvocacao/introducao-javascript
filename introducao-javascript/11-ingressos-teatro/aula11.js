ca190c7059ca6aa7a02661e9d370698a6ad38e11
pedro <leonardosos30@gmail.com>
2026-08-20 16:52:29 -0300

    upedad

--- introducao-javascript/11-ingressos-teatro/aula11.js
+++ introducao-javascript/11-ingressos-teatro/aula11.js
@@ -1,7 +1,17 @@
 const totalCompra = 180;
 let desconto = 0;
-if (totalCompra > 200) { desconto = 0.15;} else if (totalCompra >= 100) { desconto = 0.05;}
-const valorComDesconto + totalCompra  * (1 - desconto);
-const frete =  totalCompra > valorComDesconto + frete;
 
-document.getElementById("resltado"). textContent = 'Total a paga: R$ ${totalFinal.fixed(2)}';
+if (totalCompra > 200) {
+    desconto = 0.15;
+} else if (totalCompra >= 100) {
+    desconto = 0.05;
+}
+
+const valorComDesconto = totalCompra * (1 - desconto);
+
+const frete = totalCompra > 150 ? 0 : 20;
+
+const totalFinal = valorComDesconto + frete;
+
+document.getElementById("resultado").textContent =
+    `Total a pagar: R$ ${totalFinal.toFixed(2)}`;
